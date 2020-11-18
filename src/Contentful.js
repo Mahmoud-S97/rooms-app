2d14531c822ebd69794e228e03017cf0605a1c04
Mahmoud Saleh <47014640+Mahmoud-S97@users.noreply.github.com>
2020-11-18 16:38:17 +0200

    Update Contentful.js

--- src/Contentful.js
+++ src/Contentful.js
@@ -2,10 +2,10 @@ import { createClient } from 'contentful';
 
 export default createClient({ // We got this data of the capital letters from the .env.development file
     // This is the space ID. A space is like a project folder in Contentful terms
-    space: process.env.REACT_APP_API_SPACE,
-//     space: "hwqgwk5bcoh6",
+//     space: process.env.REACT_APP_API_SPACE,
+    space: "hwqgwk5bcoh6",
 
     // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-    accessToken: process.env.REACT_APP_API_SPACE
-//     accessToken: "2YahAunW7dEieha7Rt5aat8bCxXgOsel17ekhvqjklk"
+//     accessToken: process.env.REACT_APP_API_SPACE
+    accessToken: "2YahAunW7dEieha7Rt5aat8bCxXgOsel17ekhvqjklk"
   });
